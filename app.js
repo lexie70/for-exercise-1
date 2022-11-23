acc90512e7b54ae1931e99e11c884c367e521e23
lexie70 <alexandracampos658@gmail.com>
2022-11-23 02:44:08 +0000

    test

---
+++ app.js
@@ -0,0 +1,22 @@
+let pronoun = ["the","our"];
+let adj = ["great","big"];
+let noun =["jogger","racoon"];
+
+function testing(){
+for(let i = 0; i < pronoun.lenght; i++){
+    for(let j = 0; j < adj.lenght; j++){
+        for(let k = 0; k < noun.lenght; k++){
+            console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+
+        }
+     }
+   }
+}
+
+testing()
+    
+     
+     
+       
+  
+
